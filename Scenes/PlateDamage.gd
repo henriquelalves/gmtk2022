379205c19719b7b91ac518edc3cf3603cc21f233
Victor Andrade de Almeida <vct.a.almeida@gmail.com>
2022-07-17 13:20:01 -0300

    Add damage sfx

--- Scenes/PlateDamage.gd
+++ Scenes/PlateDamage.gd
@@ -2,7 +2,7 @@ extends Plate
 
 func activate(entity : Entity, dungeon):
 	if entity is Player:
-		entity.add_action("cor_shake", [0.2])
+		entity.add_action("cor_damage", [0.2])
 		Global.turns -= int(entity.get_top())
 		return
 
